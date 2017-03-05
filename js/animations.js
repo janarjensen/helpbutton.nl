205474cf9e42f948dad7a54d45c4cd3b8fecd5b2
Arjen Janssen <arjenjanssen@gmail.com>
2017-03-05 14:33:48 +0100

    buy button added

--- js/animations.js
+++ js/animations.js
@@ -8,6 +8,13 @@ $(function() {
     $('.functionText').matchHeight();
 });
 
+// make buy button appear
+$(window).load(function() {
+    setTimeout(function() {
+        $(".buyNow").show('fadeIn', {}, 1000)
+    }, 2000);
+});
+
 // // toggle class on click
 // $("**clickable div id here**").on("click", function(){
 //   $("**div id to recieve togge here**").toggleClass("**class1 class2**");
