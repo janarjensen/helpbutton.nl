c5b38256216343910a28b67e08d7403704362970
Arjen Janssen <arjenjanssen@gmail.com>
2017-03-05 15:53:59 +0100

    wtf

--- js/animations.js
+++ js/animations.js
@@ -20,3 +20,21 @@ $(window).load(function() {
 //   $("**div id to recieve togge here**").toggleClass("**class1 class2**");
 // });
 //
+$(document).ready(function () {
+  $('.accordion-tabs').each(function(index) {
+    $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+  });
+  $('.accordion-tabs').on('click', 'li > a.tab-link', function(event) {
+    if (!$(this).hasClass('is-active')) {
+      event.preventDefault();
+      var accordionTabs = $(this).closest('.accordion-tabs');
+      accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+      $(this).next().toggleClass('is-open').toggle();
+      accordionTabs.find('.is-active').removeClass('is-active');
+      $(this).addClass('is-active');
+    } else {
+      event.preventDefault();
+    }
+  });
+});
